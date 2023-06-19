9901b90a2e8284a1702eb1bc06ad2a64abc13877
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-19 11:58:19 +0530

    user data of rabbitmq

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /opt
-git clone https://github.com/PranathiAT/Roboshop-shell
+git clone https://github.com/PranathiAT/Roboshop-shell.git
 cd roboshop-shell
 bash rabbitmq.sh ${rabbitmq_appuser_password} &>>/opt/roboshop.log
